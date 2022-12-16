86bb8686249733792851a53e4b415d26c4b80c05
Wilson <leevincentwilson@gmail.com>
2022-12-16 11:58:11 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-try {
+
+const code = async()=>{
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 
     const token = core.getInput('token')
     const context = github.context
     const git = github.getOctokit(token)
-        git.rest.repos.createRelease({
-            owner: core.getInput('owner'),
-            repo: core.getInput('repo'),
-            tag_name: "1.2"
-        })
+    await git.rest.repos.createRelease({
+        owner: core.getInput('owner'),
+        repo: core.getInput('repo'),
+        tag_name: "1.2"
+    })
+    console.log('done')
+}
+try {
+  code()
+
+
 
 } catch (error) {
     // @ts-ignore
