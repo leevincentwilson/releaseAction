f8a5d1f2cec193d1e470b62e75b871bb4ed471bf
Wilson <leevincentwilson@gmail.com>
2022-12-16 12:03:56 +0000

    updates

--- dist/index.js
+++ dist/index.js
@@ -9535,18 +9535,39 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
-try {
+const code = () => __awaiter(void 0, void 0, void 0, function* () {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+    const token = core.getInput('token');
+    const context = github.context;
+    const git = github.getOctokit(token);
+    yield git.rest.repos.createRelease({
+        owner: core.getInput('owner'),
+        repo: core.getInput('repo'),
+        tag_name: "1.2"
+    });
+    console.log('done1');
+});
+try {
+    code();
 }
 catch (error) {
     // @ts-ignore
