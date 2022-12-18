697d9b2d2ef1e50640b0b2344cc840ab543e96e3
Wilson <leevincentwilson@gmail.com>
2022-12-18 00:56:20 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,33 @@ async function run() {
 const createRelease = async () =>{
     const token = core.getInput('token')
     const git = github.getOctokit(token)
-    git.rest.repos.createRelease({
+
+
+    type Values = {
+        owner: string,
+        repo: string,
+        tag_name: string,
+        target_commitish?: string,
+        name?: string,
+        body?: string,
+        draft: boolean,
+        prerelease: boolean,
+        discussion_category_name?: string,
+        generate_release_notes: boolean
+    }
+
+    const {
+        owner,
+        repo,
+        tag_name,
+        target_commitish,
+        name,
+        body,
+        draft,
+        prerelease,
+        discussion_category_name,
+        generate_release_notes
+    }:Values= {
         owner: core.getInput('owner') || github.context.repo.owner,
         repo: github.context.repo.repo || github.context.repo.repo,
         tag_name: core.getInput('tag'),
@@ -25,6 +51,21 @@ const createRelease = async () =>{
         prerelease: core.getInput('prerelease') ==='true',
         discussion_category_name: core.getInput('discussion_category_name'),
         generate_release_notes: core.getInput('generate_release_notes') === 'true'
+    }
+
+    git.rest.repos.createRelease({
+        owner,
+        repo,
+        tag_name,
+        ...(target_commitish && {target_commitish}),
+        ...(name && {name}),
+        ...(body && {body}),
+        draft,
+        prerelease,
+        ...(discussion_category_name && {discussion_category_name}),
+        generate_release_notes
+
+
     })
 }
 run()
